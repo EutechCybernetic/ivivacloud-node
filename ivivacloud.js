54a0759dee8b4ca54c5a909a4e04eaca288ae7c7
Haran Shivanan <haran@ecyber.com>
2016-08-09 17:07:09 +0530

    Fixed tab/spacing. Fixed messagebus connectivity for virtual dir hosting

--- ivivacloud.js
+++ ivivacloud.js
@@ -5,96 +5,98 @@ var signalR = require('signalr-client');
 
 function Account(host,apiKey){
 
-	this.apiKey = apiKey;
-	this.host = host;
+    this.apiKey = apiKey;
+    this.host = host;
 }
 
 Account.prototype.executeService = function executeService(service,parameters,callback){
-	try{
+    try {
 
-		service = service.replace(':','/').replace('.','/');
-		var urlObj = url.parse(this.host);
-		if (urlObj.path == '/') urlObj.path = '';
+        service = service.replace(':','/').replace('.','/');
+        var urlObj = url.parse(this.host);
+        if (urlObj.path == '/') urlObj.path = '';
 
-		var data = querystring.stringify(parameters);
+        var data = querystring.stringify(parameters);
 
-		var options = {
-			host: urlObj.hostname,
-			port: urlObj.port,
-			path: urlObj.path + '/api/' + service,
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': Buffer.byteLength(data),
-				'Authorization': this.apiKey
-			},
-		};
-		var result = '';
-		var req = http.request(options,function(res){
-			var status = res.statusCode;
-			res.setEncoding('utf8');
-			res.on('data',function(chunk){
-				result += chunk;
-			});
-			res.on('error',function(e){
-				callback(e,null);
-			});
-			res.on('end',function(){
-				if (status >= 400) {
-					callback({message:result,status:status},null);
-				} else {
-				callback(null,result);
-				}
-			});
-		});
-		req.on('error',function(e){
-			callback(e,null);
-		});
-		req.write(data);
-		req.end();
-	}catch(err){
-		callback(err,null);
-	}
+        var options = {
+            host: urlObj.hostname,
+            port: urlObj.port,
+            path: urlObj.path + '/api/' + service,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(data),
+                'Authorization': this.apiKey
+            },
+        };
+        var result = '';
+        var req = http.request(options,function(res){
+            var status = res.statusCode;
+            res.setEncoding('utf8');
+            res.on('data',function(chunk){
+                result += chunk;
+            });
+            res.on('error',function(e){
+                callback(e,null);
+            });
+            res.on('end',function(){
+                if (status >= 400) {
+                    callback({message:result,status:status},null);
+                } else {
+                callback(null,result);
+                }
+            });
+        });
+        req.on('error',function(e){
+            callback(e,null);
+        });
+        req.write(data);
+        req.end();
+    }catch(err){
+        callback(err,null);
+    }
 
 };
 
  function MessageBus(account){
 
-	var client = null;
+    var client = null;
 
- 	var Hubs = ['GlobalHub'];
+    var Hubs = ['GlobalHub'];
 
-	var subscriptions = {};
+    var subscriptions = {};
 
-	var signalRUrl = account.host + '/signalR';
+    var host = url.parse(account.host);
+    host.pathname = '/signalR';
+    var signalRUrl = host.format();
 
-	var apikey = account.apiKey;
+    var apikey = account.apiKey;
 
-	this.subscribe = function(channel,callback2){
-		subscriptions[channel] = callback2;
-		client.invoke(Hubs[0],'subscribe',apikey,channel);
-	};
+    this.subscribe = function(channel,callback2){
+        subscriptions[channel] = callback2;
+        client.invoke(Hubs[0],'subscribe',apikey,channel);
+    };
 
-	this.broadcast = function(channel,message){
-		client.invoke(Hubs[0],'broadcast',apikey,channel,message);
-	};
+    this.broadcast = function(channel,message){
+        client.invoke(Hubs[0],'broadcast',apikey,channel,message);
+    };
 
 
-	this.init = function(callback){
-		client = new signalR.client(signalRUrl,Hubs,2,true);
-		client.start();
-		client.handlers.globalhub = { broadcast: function(channel,message){
-			if(subscriptions[channel]!=null){
-				subscriptions[channel](channel,message);
-			}else{
-				console.log('err:'+channel);
-			}
-		}};
+    this.init = function(callback){
+        client = new signalR.client(signalRUrl,Hubs,2,true);
+        client.start();
+        client.handlers.globalhub = { broadcast: function(channel,message){
+            if(subscriptions[channel]!=null){
+                subscriptions[channel](channel,message);
+            }else{
+                console.log('err:'+channel);
+            }
+        }};
 
-		client.serviceHandlers.connected = function(connection) {
-			callback();
-		};
-	};
+        client.serviceHandlers.connected = function(connection) {
+            callback();
+        };
+    };
 }
 
 module.exports.Account = Account;
